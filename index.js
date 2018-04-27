40b6a5659a8ae2653606eff02d887358af3b8563
svhuynh <steven-vuong.huynh@polymtl.ca>
2018-04-27 01:18:45 -0400

    Added basic bot

---
+++ index.js
@@ -0,0 +1,24 @@
+const tokenConfig = require("./token.json");
+const botConfig = require("./config.json");
+const Discord = require('discord.js');
+
+const client  = new Discord.Client();
+
+client.on('ready', async () => {
+    console.log(`${client.user.username} is online!`);
+});
+
+// On message reception
+client.on('message', async message => {
+    let prefix = botConfig.prefix;
+    // Ignore mot messages
+    if(message.author.bot) return;
+    // Ignore private messages
+    if(message.channel.type === "dm") return;
+
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0]
+
+});
+
+client.login(tokenConfig.token);
